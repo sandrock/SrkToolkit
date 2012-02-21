0ba24f2355e27958d53c99138121bfcb053c27eb
SandRock <antoine.sottiau@gmail.com>
2012-02-21 18:09:15 +0000

    AppEventBus

---
+++ Sources/NET4.SrkToolkit.Services.Tests/AppEventBusTest.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SrkToolkit.Services.Tests
+{
+    /// <summary>
+    ///This is a test class for <see cref="AppEventBus"/> and is intended
+    ///to contain all <see cref="AppEventBus"/> Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class AppEventBusTest {
+        [TestClass]
+        public class RegisterMethod {
+            [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+            public void ValidatesInputs0() {
+                Action<object, DateTime> action = null;
+                new AppEventBus().Register(action);
+            }
+
+            [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+            public void ValidatesInputs1() {
+                Action<object, DateTime> action = (s, e) => { };
+                new AppEventBus().Register(action, null);
+            }
+
+            [TestMethod]
+            public void ReturnsDisposable0() {
+                Action<object, DateTime> action = (s, e) => { };
+                var result = new AppEventBus().Register(action);
+
+                Assert.IsNotNull(result);
+                result.Dispose();
+                Assert.IsTrue(result.IsDisposed);
+            }
+
+            [TestMethod]
+            public void ReturnsDisposable1() {
+                Action<object, DateTime> action = (s, e) => { };
+                var result = new AppEventBus().Register(action, new object());
+
+                Assert.IsNotNull(result);
+                result.Dispose();
+                Assert.IsTrue(result.IsDisposed);
+            }
+        }
+
+        [TestClass]
+        public class PublishMethod {
+            [TestMethod]
+            public void DoesNothingIfNoRegistrations() {
+                new AppEventBus().Publish(new object(), new object());
+            }
+
+            [TestMethod]
+            public void PublishesToAllRegistered() {
+                // prepare
+                bool a = false, b = false;
+                var bus = new AppEventBus();
+
+                // execute
+                bus.Register<DateTime>((s1, e1) => a = true);
+                bus.Register<DateTime>((s2, e2) => b = true);
+                bus.Publish(new object(), DateTime.Now);
+
+                // verify
+                Assert.IsTrue(a);
+                Assert.IsTrue(b);
+            }
+
+            [TestMethod]
+            public void DoesNotPublishToOthers() {
+                // prepare
+                bool a = false, b = false, c = false;
+                var bus = new AppEventBus();
+
+                // execute
+                bus.Register<DateTime>((s1, e1) => a = true);
+                bus.Register<DateTime>((s2, e2) => b = true);
+                bus.Register<TimeSpan>((s3, e3) => c = true);
+                bus.Publish(new object(), DateTime.Now);
+
+                // verify
+                Assert.IsTrue(a);
+                Assert.IsTrue(b);
+                Assert.IsFalse(c);
+            }
+
+            [TestMethod]
+            public void DoesNotPublishToSelf() {
+                // prepare
+                object o1 = new object();
+                bool a = false, b = false, c = false;
+                var bus = new AppEventBus();
+
+                // execute
+                bus.Register<DateTime>((s1, e1) => a = true);
+                bus.Register<DateTime>((s2, e2) => b = true, o1);
+                bus.Register<DateTime>((s3, e3) => c = true);
+                bus.Publish(o1, DateTime.Now);
+
+                // verify
+                Assert.IsTrue(a);
+                Assert.IsFalse(b);
+                Assert.IsTrue(c);
+            }
+
+            [TestMethod]
+            public void WorksManyTimes() {
+                // prepare
+                object o1 = new object();
+                bool a = false, b = false, c = false;
+                var bus = new AppEventBus();
+
+                // execute
+                bus.Register<DateTime>((s1, e1) => a = true);
+                bus.Register<DateTime>((s2, e2) => b = true, o1);
+                bus.Register<TimeSpan>((s3, e3) => c = true);
+                bus.Publish(o1, DateTime.Now);
+
+                // verify
+                Assert.IsTrue(a);
+                Assert.IsFalse(b);
+                Assert.IsFalse(c);
+
+                // execute
+                a = b = c = false;
+                bus.Publish(o1, DateTime.Now);
+
+                // verify
+                Assert.IsTrue(a);
+                Assert.IsFalse(b);
+                Assert.IsFalse(c);
+
+                // execute
+                a = b = c = false;
+                bus.Publish(o1, DateTime.Now);
+
+                // verify
+                Assert.IsTrue(a);
+                Assert.IsFalse(b);
+                Assert.IsFalse(c);
+            }
+        }
+    }
+}
---
+++ Sources/NET4.SrkToolkit.Services/AppEventBus.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SrkToolkit.Services {
+    public class AppEventBus {
+        internal class Subscription : IDisposed {
+            internal Guid EventType { get; set; }
+            internal object Action { get; set; }
+            internal AppEventBus Bus { get; set; }
+            internal object Registree { get; set; }
+            void IDisposable.Dispose() {
+                if (this.Bus != null)
+                    this.Bus.Unregister(this);
+                this.Bus = null;
+            }
+            bool IDisposed.IsDisposed {
+                get { return this.Bus == null; }
+            }
+        }
+
+        private static AppEventBus instance;
+        private readonly Dictionary<Guid, List<Subscription>> subscriptions = new Dictionary<Guid, List<Subscription>>();
+
+        public AppEventBus() {
+        }
+
+        public static AppEventBus Instance {
+            get { return instance ?? (instance = new AppEventBus()); }
+        }
+
+        private List<Subscription> GetListForType(Guid id) {
+            if (!subscriptions.ContainsKey(id))
+                subscriptions[id] = new List<Subscription>();
+            return subscriptions[id];
+        }
+
+        public void Publish<T>(object sender, T args) {
+            var id = typeof(T).GUID;
+            foreach (var item in GetListForType(id)) {
+                if (item.Registree != null && item.Registree == sender)
+                    continue;
+
+                var evt = (Action<object, T>)item.Action;
+                evt.Invoke(sender, args);
+            }
+        }
+
+        public IDisposed Register<T>(Action<object, T> action) {
+            if (action == null)
+                throw new ArgumentNullException("action");
+
+            var id = typeof(T).GUID;
+            var reg = new Subscription {
+                Bus = this, Action = action, EventType = id
+            };
+            this.GetListForType(id).Add(reg);
+            return reg;
+        }
+
+        public IDisposed Register<T>(Action<object, T> action, object registree) {
+            if (action == null)
+                throw new ArgumentNullException("action");
+            if (registree == null)
+                throw new ArgumentNullException("registree");
+
+            var id = typeof(T).GUID;
+            var reg = new Subscription {
+                Bus = this, Action = action, EventType = id, Registree = registree
+            };
+            this.GetListForType(id).Add(reg);
+            return reg;
+        }
+    
+        internal void Unregister(Subscription subscription)
+        {
+            var list = GetListForType(subscription.EventType);
+            if (list.Contains(subscription))
+                list.Remove(subscription);
+        }
+    }
+}

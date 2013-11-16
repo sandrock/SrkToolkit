fa730897a07582700812d86a3d1f5af9db54707a
SandRock <antoine.sottiau@gmail.com>
2013-11-16 17:49:34 +0000

    string.NullIfEmpty() string.NullIfEmptyOrWhitespace()

---
+++ Sources/NET4.SrkToolkit.Common.Tests/SrkStringExtensionsTests.cs
@@ -0,0 +1,84 @@
+﻿
+namespace SrkToolkit.Common.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    public class SrkStringExtensionsTests
+    {
+        [TestClass]
+        public class NullIfEmptyMethod
+        {
+            [TestMethod]
+            public void Null()
+            {
+                Assert.IsNull(SrkStringExtensions.NullIfEmpty(null));
+            }
+
+            [TestMethod]
+            public void Empty()
+            {
+                Assert.IsNull(SrkStringExtensions.NullIfEmpty(""));
+            }
+
+            [TestMethod]
+            public void Whitespace()
+            {
+                Assert.IsNotNull(SrkStringExtensions.NullIfEmpty("   \t\r\n"));
+            }
+
+            [TestMethod]
+            public void Text()
+            {
+                string value = " hello ";
+                Assert.AreEqual(value, SrkStringExtensions.NullIfEmpty(value));
+            }
+
+            [TestMethod]
+            public void TextTrim()
+            {
+                string value = " hello ";
+                Assert.AreEqual("hello", SrkStringExtensions.NullIfEmpty(value, true));
+            }
+        }
+
+        [TestClass]
+        public class NullIfEmptyOrWhitespaceMethod
+        {
+            [TestMethod]
+            public void Null()
+            {
+                Assert.IsNull(SrkStringExtensions.NullIfEmptyOrWhitespace(null));
+            }
+
+            [TestMethod]
+            public void Empty()
+            {
+                Assert.IsNull(SrkStringExtensions.NullIfEmptyOrWhitespace(""));
+            }
+
+            [TestMethod]
+            public void Whitespace()
+            {
+                Assert.IsNull(SrkStringExtensions.NullIfEmptyOrWhitespace("   \t\r\n"));
+            }
+
+            [TestMethod]
+            public void Text()
+            {
+                string value = " hello ";
+                Assert.AreEqual(value, SrkStringExtensions.NullIfEmptyOrWhitespace(value));
+            }
+
+            [TestMethod]
+            public void TextTrim()
+            {
+                string value = " hello ";
+                Assert.AreEqual("hello", SrkStringExtensions.NullIfEmptyOrWhitespace(value, true));
+            }
+        }
+    }
+}
---
+++ Sources/NET4.SrkToolkit.Common/System/SrkStringExtensions.cs
@@ -0,0 +1,42 @@
+﻿
+namespace System
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// Extension methods for <see cref="string"/>.
+    /// </summary>
+    public static class SrkStringExtensions
+    {
+        /// <summary>
+        /// Returns null is the string is empty.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        public static string NullIfEmpty(this string value, bool trim = false)
+        {
+            if (string.IsNullOrEmpty(value))
+                return null;
+            if (trim)
+                return value.Trim();
+            return value;
+        }
+
+        /// <summary>
+        /// Returns null is the string is empty or contains only whispaces.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        public static string NullIfEmptyOrWhitespace(this string value, bool trim = false)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                return null;
+            if (trim)
+                return value.Trim();
+            return value;
+        }
+    }
+}

b99916260ed3b82d777dee9e6af5fff040e23acd
SandRock <antoine.sottiau@gmail.com>
2014-11-16 15:13:53 +0000

    Added CultureInfoAttribute.

---
+++ Sources/NET4.SrkToolkit.Common.Tests/DataAnnotations/CultureInfoAttributeTests.cs
@@ -0,0 +1,118 @@
+﻿
+namespace SrkToolkit.Common.Tests.DataAnnotations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using SrkToolkit.DataAnnotations;
+
+    [TestClass]
+    public class CultureInfoAttributeTests
+    {
+        [TestMethod]
+        public void ReturnsFalseWithEmptyValue()
+        {
+            var attr = new CultureInfoAttribute();
+            string value = string.Empty;
+            bool expected = false;
+            bool result = attr.IsValid(value);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ReturnsFalseWithNullValue()
+        {
+            var attr = new CultureInfoAttribute();
+            string value = null;
+            bool expected = false;
+            bool result = attr.IsValid(value);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ReturnsTrueWithValidValue()
+        {
+            var attr = new CultureInfoAttribute();
+            string value = "fr-FR";
+            bool expected = true;
+            bool result = attr.IsValid(value);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void AllowNeutral_ReturnsTrueWithNeutral()
+        {
+            var attr = new CultureInfoAttribute();
+            attr.AllowNeutralCulture = true;
+            string value = "fr";
+            bool expected = true;
+            bool result = attr.IsValid(value);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void AllowNeutral_ReturnsTrueWithNonNeutral()
+        {
+            var attr = new CultureInfoAttribute();
+            attr.AllowNeutralCulture = true;
+            string value = "fr-CA";
+            bool expected = true;
+            bool result = attr.IsValid(value);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void DisallowNeutral_ReturnsFalseWithNeutral()
+        {
+            var attr = new CultureInfoAttribute();
+            attr.AllowNeutralCulture = false;
+            string value = "fr";
+            bool expected = false;
+            bool result = attr.IsValid(value);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void DisallowNeutral_ReturnsTrueWithNonNeutral()
+        {
+            var attr = new CultureInfoAttribute();
+            attr.AllowNeutralCulture = false;
+            string value = "fr-CA";
+            bool expected = true;
+            bool result = attr.IsValid(value);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void DoNotAllowNeutralByDefault()
+        {
+            var attr = new CultureInfoAttribute();
+            Assert.IsFalse(attr.AllowNeutralCulture);
+        }
+
+        [TestMethod]
+        public void AllowNeutral_ReturnsFalseWithInvariant()
+        {
+            var attr = new CultureInfoAttribute();
+            attr.AllowNeutralCulture = true;
+            string value = CultureInfo.InvariantCulture.Name;
+            bool expected = false;
+            bool result = attr.IsValid(value);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void DisallowNeutral_ReturnsFalseWithInvariant()
+        {
+            var attr = new CultureInfoAttribute();
+            attr.AllowNeutralCulture = false;
+            string value = CultureInfo.InvariantCulture.Name;
+            bool expected = false;
+            bool result = attr.IsValid(value);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Sources/NET4.SrkToolkit.Common/DataAnnotations/CultureInfoAttribute.cs
@@ -0,0 +1,70 @@
+﻿
+namespace SrkToolkit.DataAnnotations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+    using SrkToolkit.Resources;
+
+    /// <summary>
+    /// Validates the field is a valid <see cref="CultureInfo"/> name.
+    /// </summary>
+    public class CultureInfoAttribute : ValidationAttribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CultureInfoAttribute"/> class.
+        /// </summary>
+        public CultureInfoAttribute()
+        {
+            this.ErrorMessageResourceName = "CultureInfoAttribute_ErrorMessage";
+            this.ErrorMessageResourceType = typeof(Strings);
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to allow neutral culture.
+        /// </summary>
+        public bool AllowNeutralCulture { get; set; }
+
+        /// <summary>
+        /// Determines whether the specified value of the object is valid.
+        /// </summary>
+        /// <param name="value">The value of the object to validate.</param>
+        /// <returns>
+        /// true if the specified value is valid; otherwise, false.
+        /// </returns>
+        public override bool IsValid(object value)
+        {
+            string val;
+            if (value == null || string.IsNullOrEmpty(val = value.ToString()))
+                return false;
+
+            try
+            {
+                var culture = new CultureInfo(val);
+
+                if (culture == null || culture.Equals(CultureInfo.InvariantCulture))
+                    return false;
+
+                if (this.AllowNeutralCulture)
+                {
+                    return true;
+                }
+                else
+                {
+                    return !culture.IsNeutralCulture;
+                }
+            }
+            catch (CultureNotFoundException)
+            {
+                return false;
+            }
+            catch (ArgumentException)
+            {
+                return false;
+            }
+        }
+    }
+}

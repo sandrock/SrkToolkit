707aecd451974453805014cee1a416874bea4d8a
sandrock <antoine.sottiau@gmail.com>
2016-09-02 16:56:42 +0200

    Implemented #4: Create extension IDictionary<K, V>.GetValue() that returns null when the key does not exist

---
+++ Sources/NET4.SrkToolkit.Common/System.Collections.Generic/SrkIDictionaryExtensions.cs
@@ -0,0 +1,29 @@
+﻿
+namespace System.Collections.Generic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class SrkIDictionaryExtensions
+    {
+        /// <summary>
+        /// Gets a value or null. Does not throw KeyNotFoundException.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dictionary"></param>
+        /// <param name="key"></param>
+        /// <returns>the value or null</returns>
+        public static TValue GetValue<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
+        {
+            if (dictionary == null)
+                throw new ArgumentNullException("dictionary");
+
+            TValue value;
+            if (dictionary.TryGetValue(key, out value))
+                value = default(TValue);
+
+            return value;
+        }
+    }
+}

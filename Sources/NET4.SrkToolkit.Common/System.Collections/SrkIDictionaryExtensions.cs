58ba8e997d0901ad4d08258fcc36767100666540
sandrock <antoine.sottiau@gmail.com>
2016-09-02 16:58:23 +0200

    Implemented #4: Create extension IDictionary<K, V>.GetValue() that returns null when the key does not exist

--- Sources/NET4.SrkToolkit.Common/System.Collections/SrkIDictionaryExtensions.cs
+++ Sources/NET4.SrkToolkit.Common/System.Collections/SrkIDictionaryExtensions.cs
@@ -4,7 +4,6 @@ namespace System.Collections
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
 
     /// <summary>
     /// Extension methods for the <see cref="IDictionary"/> interface.

6231472ec83ff3c072646b5dc42f9e509f654eff
sandrock <antoine.sottiau@gmail.com>
2017-01-21 14:26:08 +0100

    StringComparerEx

---
+++ Sources/NET4.SrkToolkit.Common/StringComparerEx.cs
@@ -0,0 +1,53 @@
+﻿
+namespace SrkToolkit.Common
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// A string comparer class that accepts a <see cref="CompareOptions"/> argument.
+    /// </summary>
+    /// <example>
+    /// // this comparer will ignore non-spacing characters (diacritics and co)
+    /// var stringComparer = new StringComparerEx(CultureInfo.InvariantCulture, CompareOptions.IgnoreCase | CompareOptions.IgnoreNonSpace);
+    /// </example>
+    public class StringComparerEx : IEqualityComparer<string>
+    {
+        private readonly CultureInfo culture;
+        private readonly CompareOptions options;
+
+        public StringComparerEx(CultureInfo culture, CompareOptions options)
+        {
+            this.culture = culture;
+            this.options = options;
+        }
+
+        public bool Equals(string x, string y)
+        {
+            if (x == y)
+            {
+                return true;
+            }
+
+            if (x == null || y == null)
+            {
+                return false;
+            }
+
+            if (x.Length != y.Length)
+            {
+                return false;
+            }
+
+            return string.Compare(x, y, this.culture, this.options) == 0;
+        }
+
+        public int GetHashCode(string obj)
+        {
+            return 0; // force usage of the Equals method
+        }
+    }
+}
---
+++ Sources/NET45.SrkToolkit.Common.Tests/StringComparerExTests.cs
@@ -0,0 +1,50 @@
+﻿
+namespace SrkToolkit.Common.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+
+    [TestClass]
+    public class StringComparerExTests
+    {
+        [TestMethod]
+        public void StringComparerEx_AccentedStringAreEqual1()
+        {
+            string a = "écoûtèr", b = "ëcöutêr";
+            var stringComparer = new StringComparerEx(CultureInfo.InvariantCulture, CompareOptions.IgnoreCase | CompareOptions.IgnoreNonSpace);
+            bool result = stringComparer.Equals(a, b);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void StringComparerEx_AccentedStringAreEqual2()
+        {
+            string a = "écoûtèr", b = "écoûtèr";
+            var stringComparer = new StringComparerEx(CultureInfo.InvariantCulture, CompareOptions.IgnoreCase | CompareOptions.IgnoreNonSpace);
+            bool result = stringComparer.Equals(a, b);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void StringComparerEx_AccentedStringAreEqual3()
+        {
+            string a = "écoûtèr", b = "ecouter";
+            var stringComparer = new StringComparerEx(CultureInfo.InvariantCulture, CompareOptions.IgnoreCase | CompareOptions.IgnoreNonSpace);
+            bool result = stringComparer.Equals(a, b);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void StringComparerEx_DifferentStringAreDifferent()
+        {
+            string a = "acoutur", b = "ëcöutêr";
+            var stringComparer = new StringComparerEx(CultureInfo.InvariantCulture, CompareOptions.IgnoreCase | CompareOptions.IgnoreNonSpace);
+            bool result = stringComparer.Equals(a, b);
+            Assert.IsFalse(result);
+        }
+    }
+}

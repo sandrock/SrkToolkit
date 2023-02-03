d23be1809fd47a6a4088799789c0d5e5b2e32c93
SandRock <sandrock@sandrock.fr>
2023-02-03 15:41:15 +0100

    component testing: convert one project to Xunit

---
+++ Sources/NET45.SrkToolkit.Common.Tests/DictionaryWrapperTests.cs
@@ -0,0 +1,27 @@
+﻿
+namespace SrkToolkit.Common.Tests
+{
+    using SrkToolkit.Collections;
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using Xunit;
+
+    public class DictionaryWrapperTests
+    {
+        [Fact]
+        public void Works()
+        {
+            var source = new Hashtable();
+            source.Add("a", "b");
+            source.Add("b", "c");
+            var target = new DictionaryWrapper<string, string>(source);
+            Assert.Equal(source.Count, target.Count);
+            Assert.Equal("b", target["a"]);
+            Assert.Equal("c", target["b"]);
+
+            target.Add("x", "y");
+            Assert.Equal(3, source.Count);
+        }
+    }
+}
